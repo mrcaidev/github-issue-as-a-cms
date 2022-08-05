e2ee177412666623e79ae1cd7203d33a71877a18
mrcaidev <mrcaidev@qq.com>
2022-08-05 18:59:52 +0800

    fix(component): add page title for 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,23 @@
+import Head from "next/head";
 import { Link } from "src/components/common/link";
 
 const Page = () => (
-  <main className="flex flex-col items-center gap-y-4 m-auto translate-y-16">
-    <h1 className="text-7xl sm:text-8xl lg:text-9xl font-black transition-colors">
-      404
-    </h1>
-    <p className="text-lg sm:text-xl lg:text-2xl transition-colors">
-      Oops! This page does not exist!
-    </p>
-    <Link href="/" className="ghost px-4">
-      Back to home
-    </Link>
-  </main>
+  <>
+    <Head>
+      <title>404 - MrCai</title>
+    </Head>
+    <main className="flex flex-col items-center gap-y-4 m-auto translate-y-16">
+      <h1 className="text-7xl sm:text-8xl lg:text-9xl font-black transition-colors">
+        404
+      </h1>
+      <p className="text-lg sm:text-xl lg:text-2xl transition-colors">
+        Oops! This page does not exist!
+      </p>
+      <Link href="/" className="ghost px-4">
+        Back to home
+      </Link>
+    </main>
+  </>
 );
 
 export default Page;
