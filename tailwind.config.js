c6a97288a4bade8cd962e5c4d36013a5ef43d941
mrcaidev <mrcaidev@qq.com>
2022-08-05 23:27:48 +0800

    build(dep): add @tailwind/typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,5 +12,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/line-clamp")],
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+  ],
 };
