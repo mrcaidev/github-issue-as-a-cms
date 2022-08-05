0032dc9539f594229bf3cae3e589b0ae48163721
mrcaidev <mrcaidev@qq.com>
2022-08-05 13:59:49 +0800

    refactor(util): assume full front matter

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -2,7 +2,6 @@ import { readdir, readFile } from "fs/promises";
 import matter from "gray-matter";
 import { join } from "path";
 import { cwd } from "process";
-import { DEFAULT_DESCRIPTION, DEFAULT_TITLE, DEFAULT_TOPIC } from "./constants";
 
 const postsDirectory = join(cwd(), "posts");
 
@@ -16,28 +15,12 @@ export interface IPost {
 }
 
 export async function getPostByFilename(filename: string) {
+  const path = filename.replace(/\.md$/, "");
   const file = join(postsDirectory, filename);
   const fileContent = await readFile(file, { encoding: "utf-8" });
   const { data, content } = matter(fileContent);
 
-  const path = filename.replace(/\.md$/, "");
-  const createdAt = new Date(data.createdAt).toLocaleDateString("en", {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  });
-  const title = data.title ?? DEFAULT_TITLE;
-  const description = data.description ?? DEFAULT_DESCRIPTION;
-  const topic = data.topic ?? DEFAULT_TOPIC;
-
-  return {
-    path,
-    createdAt,
-    title,
-    description,
-    topic,
-    content,
-  } as IPost;
+  return { ...data, path, content } as IPost;
 }
 
 export async function getAllPosts() {
