32e39c892fc25d46ebce699cf740379509e5e20c
mrcaidev <mrcaidev@qq.com>
2022-08-06 16:14:04 +0800

    refactor(style): responsive title

--- src/components/common/title.tsx
+++ src/components/common/title.tsx
@@ -1,8 +1,8 @@
 import { type PropsWithChildren } from "react";
 
 export const Title = ({ children }: PropsWithChildren) => (
-  <h2 className="relative w-fit my-8 text-3xl font-bold transition-colors">
+  <h2 className="relative w-fit my-8 text-2xl sm:text-3xl font-bold transition-colors">
     {children}
-    <span className="absolute left-0 -bottom-1 w-[150%] h-0.5 rounded-full bg-sky-800 dark:bg-sky-200 transition-bg" />
+    <span className="absolute left-0 -bottom-2 w-[calc(100%+2rem)] h-1 rounded-full bg-sky-800 dark:bg-sky-200 transition-bg" />
   </h2>
 );
