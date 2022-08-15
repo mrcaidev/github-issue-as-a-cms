0d96c950ec3f4f43a1ab09f9ffde60e4ae1a87fc
mrcaidev <mrcaidev@gmail.com>
2022-08-15 19:26:01 +0800

    fix: topic card media query

--- src/components/topic-card.tsx
+++ src/components/topic-card.tsx
@@ -17,14 +17,14 @@ export const TopicCard = ({ index = 0, topic, num }: IProps) => (
     <h2>
       <Link
         href={"/topics/" + topic}
-        className="px-2 py-1 font-bold text-lg sm:text-2xl hover:text-highlight before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0"
+        className="px-2 py-1 font-bold text-lg sm:text-xl lg:text-2xl hover:text-highlight before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0"
       >
         {topic}
       </Link>
     </h2>
     <p className="text-base sm:text-lg text-dim">
       {num}
-      <span className="hidden sm:inline"> Posts</span>
+      <span className="hidden sm:inline md:hidden lg:inline"> Posts</span>
     </p>
   </div>
 );
