0eb36f424b98e0c30d1854c8af191088d7fa80e5
mrcaidev <mrcaidev@gmail.com>
2023-01-19 14:23:17 +0800

    feat: add pagination

---
+++ src/components/icons/ChevronsLeft.tsx
@@ -0,0 +1,20 @@
+import type { IconProps } from "./icon";
+
+const ChevronsLeft = (props: IconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={props.size ?? 24}
+    height={props.size ?? 24}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <polyline points="11 17 6 12 11 7" />
+    <polyline points="18 17 13 12 18 7" />
+  </svg>
+);
+
+export default ChevronsLeft;
---
+++ src/components/icons/ChevronsRight.tsx
@@ -0,0 +1,20 @@
+import type { IconProps } from "./icon";
+
+const ChevronsRight = (props: IconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={props.size ?? 24}
+    height={props.size ?? 24}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <polyline points="13 17 18 12 13 7" />
+    <polyline points="6 17 11 12 6 7" />
+  </svg>
+);
+
+export default ChevronsRight;
