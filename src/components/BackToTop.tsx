b9f16efc91f97852605d0e488f1229e627e3a24f
mrcaidev <mrcaidev@gmail.com>
2023-01-19 19:35:07 +0800

    refactor: better code quality

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -3,7 +3,7 @@ import ArrowUp from "./icons/ArrowUp";
 const BackToTop = () => (
   <button
     type="button"
-    onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+    onClick={() => scrollTo({ top: 0 })}
     aria-label="Back to top"
     class="p-2 rounded hover:bg-gray-4 dark:hover:bg-graydark-4 active:bg-gray-5 dark:active:bg-graydark-5"
   >
